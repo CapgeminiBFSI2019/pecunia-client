3499a5a111f40adfb7f2b227d2a83706dea24f42
Rohan Patil <rohanpatil365@gmail.com>
2019-10-16 13:42:52 +0530

    Change Files

--- src/app/transaction/credit-slip/credit-slip.component.ts
+++ src/app/transaction/credit-slip/credit-slip.component.ts
@@ -10,7 +10,8 @@ import { CreditSlipService } from 'src/app/service/credit-slip.service';
   styleUrls: ['./credit-slip.component.css']
 })
 export class CreditSlipComponent implements OnInit {
-
+  isProcessing : boolean = false;
+  showToast = false;
   model = new CrediSlipModel();
   dataResponse : object;
   submitted = false;
@@ -21,30 +22,25 @@ export class CreditSlipComponent implements OnInit {
 
   onDataReceived(data)
   {
-    console.log("aaya idhar"+JSON.stringify(data));
     this.dataResponse = data;
-    alert(JSON.stringify(this.dataResponse));
-    if(data["success"])
-    {
-      console.log("Success");
-    }
-    else
-    {
-      console.log(data["message"]);
-    }
+    this.showToast = true;
   }
 
   onSubmit() {
-    // alert(JSON.stringify(this.model))
+    this.isProcessing = true;
     this.submitted = true;
     this.creditService.creditAmount(this.model).subscribe(
       data => {
+        this.isProcessing = false;
         this.onDataReceived(data);
-        // console.log("Data Receieved  :" + JSON.stringify(data));
       },
       error => {
-        console.log("Error occured :" + JSON.stringify(error));
+        this.isProcessing = false;
       }
     );
+  }
+
+  closeToast() {
+    this.showToast = false;
   }
 }
