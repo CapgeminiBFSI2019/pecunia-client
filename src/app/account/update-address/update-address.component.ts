e8fb69c5ed02fc97db4b53a03b142dddeabbe7ab
Aditi1Singh <40431156+Aditi1Singh@users.noreply.github.com>
2019-10-16 16:49:10 +0530

    update account service

--- src/app/account/update-address/update-address.component.ts
+++ src/app/account/update-address/update-address.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountModel } from 'src/app/model/AccountModel';
 import { AddressModel } from 'src/app/model/AddressModel';
+import { HttpClient } from '@angular/common/http';
+import { UpdateAddressService } from 'src/app/service/update-address.service';
 
 
 @Component({
@@ -13,23 +15,39 @@ export class UpdateAddressComponent implements OnInit {
   account = new AccountModel();
   address = new AddressModel();
   submitted = false;
+  dataResponse : Object;
+  updateAddressObject: Object;
+  httpClient : HttpClient;
 
-  constructor() { }
+
+  constructor(private updateAddress : UpdateAddressService) { }
 
   ngOnInit() {
   }
 
 
-  showAccount() {
-    return JSON.stringify(this.account);
+  onDataReceived(data)
+  {
+    console.log("aaya idhar"+JSON.stringify(data));
+    // this.dataResponse = data;
+    alert(JSON.stringify(data));
+    if(data["success"])
+    {
+      console.log("Success");
+    }
+    else
+    {
+      console.log(data["message"]);
+    }
   }
-  showAddress() {
-    return JSON.stringify(this.address);
-  }
+
 
   onSubmit() {
     this.submitted = true;
-    alert(this.showAccount() + this.showAddress());
+    this.updateAddressObject = {"accountId": this.account.id, "line1": this.address.line1,
+                                "line2": this.address.line2, "city": this.address.city,
+                                "state": this.address.state, "country": this.address.country, "zipcode": this.address.zipcode};
+    this.updateAddress.doUpdate(this.updateAddressObject,this.onDataReceived);
   }
 
 
