2840913c97dc59e0308ac26a264b0f5ce0eb1be4
Rohan Patil <rohanpatil365@gmail.com>
2019-10-17 12:24:12 +0530

    Added cheque compo and service

--- src/app/transaction/credit-cheque/credit-cheque.component.ts
+++ src/app/transaction/credit-cheque/credit-cheque.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CreditChequeModel } from 'src/app/model/CreditChequeModel';
+import { CreditChequeService } from 'src/app/service/credit-cheque.service';
 
 @Component({
   selector: 'app-credit-cheque',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./credit-cheque.component.css']
 })
 export class CreditChequeComponent implements OnInit {
+  isProcessing : boolean = false;
+  showToast : boolean = false;
+  dataResponse : any;
+  model = new CreditChequeModel();
+  submitted : boolean = false;
 
-  constructor() { }
+  @ViewChild('creditChequeForm' , {static: false}) form: any;
+
+  constructor(private creditService : CreditChequeService) { }
 
   ngOnInit() {
   }
 
+  onDataReceived(data)
+  {
+    this.dataResponse = data;
+    this.showToast = true;
+  }
+  onSubmit() {
+    console.log(JSON.stringify(this.model));
+    this.isProcessing = true;
+    this.submitted = true;
+    this.creditService.creditAmount(this.model).subscribe(
+      data => {
+        this.isProcessing = false;
+        this.onDataReceived(data);
+      },
+      error => {
+        this.isProcessing = false;
+      }
+    );
+  }
+
+  closeToast() {
+    this.showToast = false;
+    this.form.reset();
+  }
 }
