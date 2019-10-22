06c6f1668c7fbda95d03cc5b4a3351f8c8e50b50
Aditi1Singh <40431156+Aditi1Singh@users.noreply.github.com>
2019-10-22 09:34:38 +0530

    removed comments and changed port to 9090

--- src/app/service/update-address.service.ts
+++ src/app/service/update-address.service.ts
@@ -12,7 +12,7 @@ export class UpdateAddressService {
   doUpdate(updateAddress): Observable<any>{
 
     return this.http
-      .post("http://localhost:8061/pecunianew/UpdateCustomerAddress",updateAddress);
+      .post("http://localhost:9090/pecunianew/UpdateCustomerAddress",updateAddress);
       
   }
 }
