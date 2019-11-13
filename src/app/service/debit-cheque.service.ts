b94570a42bb3d7ddfc12936a1fae5ed4d2563d35
basuanish <55137730+basuanish@users.noreply.github.com>
2019-11-13 15:09:59 +0530

    committed .ts file

--- src/app/service/debit-cheque.service.ts
+++ src/app/service/debit-cheque.service.ts
@@ -13,7 +13,7 @@ export class DebitChequeService {
 
    debitAmount(dataObject): Observable<any> {
     return this.http
-      .post("http://localhost:9090/pecunianew/debitCheque",dataObject);
+      .post("http://localhost:9090/debitCheque",dataObject);
       
   }
 }
